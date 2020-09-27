d483aa175c49cb1531a803b0292d27cf73c4beb8
alirueter <rueter.alison@gmail.com>
2020-09-27 10:53:33 -0500

    updated server.js

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 app.use(require('./routes'));
 
 // tell mongoose which databases to connect to
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/secret-sundaes', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
