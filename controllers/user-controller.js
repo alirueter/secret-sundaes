ab73d929cd69e3710b1f1d18d760c1fe8f33f92a
alirueter <rueter.alison@gmail.com>
2020-09-27 10:09:33 -0500

    added to user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,13 +15,65 @@ const userController = {
             console.log(err);
             res.status(500).json(err);
         });
-    }
+    },
 
     // get a single user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user was found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 
     // create a new user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
 
     // update a user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user was found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
 
     // delete a user
-}
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user was found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+
+    // add friend to user's friend list
+
+    // delete friend from user's friend list
+};
+
+module.exports = userController;
